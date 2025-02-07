c0ae989074780b5e67e4823dffc5eeae518674b0
Jerome <nietsemorej@gmail.com>
2025-02-06 18:59:49 -0800

    add jQuery

--- js/activity.js
+++ js/activity.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
     $('table td').not(':contains("Not Available")').not(':first-child').css('cursor', 'pointer');
     
-    $('table td').not(':contains("Not Available")').not(':first-child').click(function() {
-        $(this).toggleClass('highlighted');
+    $("td").click(function () {
+        var content = $(this).text();
+        
+        if (content != "Not Available") {
+            $(this).toggleClass("tdhighlight");
+            
+            if ($(this).hasClass("tdhighlight")) {
+                $('#displaySelected').css("visibility","visible");
+                $('#displaySelected').css("margin-top","2em");
+                $('#result').append("<p>"+content+"</p>");
+            } else {
+                $('#result p:contains('+content+')').remove();
+                
+                if ($('#result').has('p').length == false) {
+                    $('#displaySelected').css("visibility","hidden");
+                    $('#displaySelected').css("margin-top","0");
+                }
+            }
+        }
     });
 }); 
